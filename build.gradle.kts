bc5aafecefdf9a905341fcb8d03d12717fa4fb65
Novtopro He <novtopro@gmail.com>
2019-10-11 10:48:52 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+    id("com.github.johnrengelman.shadow") version "5.1.0"
+    kotlin("jvm") version "1.3.40"
+}
+
+group = "io.lab"
+version = "1.0-SNAPSHOT"
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    compile("io.javalin:javalin:3.5.0")
+    compile("org.apache.xmlgraphics:batik-all:1.11")
+    compile("org.scilab.forge:jlatexmath:1.0.7")
+    compile("org.slf4j:slf4j-simple:1.7.26")
+}
+
+application {
+    this.mainClassName = "io.lab.AppKt"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/kotlin/io/lab/App.kt
@@ -0,0 +1,46 @@
+package io.lab
+
+import io.javalin.Javalin
+import org.apache.batik.anim.dom.SVGDOMImplementation
+import org.apache.batik.svggen.SVGGeneratorContext
+import org.apache.batik.svggen.SVGGraphics2D
+import org.apache.batik.svggen.SVGGraphics2DIOException
+import org.scilab.forge.jlatexmath.TeXConstants
+import org.scilab.forge.jlatexmath.TeXFormula
+import java.awt.Dimension
+import java.io.ByteArrayOutputStream
+import java.io.OutputStreamWriter
+import java.io.UnsupportedEncodingException
+import java.nio.charset.StandardCharsets
+
+fun main(args: Array<String>) {
+    val app = Javalin.create().start(8080)
+    app.get("/") { ctx ->
+        ctx.contentType("image/svg+xml")
+
+        val expression = ctx.req.getParameter("exp")
+        val teXFormula = TeXFormula(expression)
+        val teXIcon = teXFormula.createTeXIcon(TeXConstants.STYLE_DISPLAY, 100f)
+
+        val document = SVGDOMImplementation.getDOMImplementation().createDocument(SVGDOMImplementation.SVG_NAMESPACE_URI, "svg", null)
+
+        val svg = SVGGraphics2D(SVGGeneratorContext.createDefault(document), true)
+        svg.svgCanvasSize = Dimension(teXIcon.iconWidth, teXIcon.iconHeight)
+
+        teXIcon.paintIcon(null, svg, 0, 0)
+
+        val outputStream = ByteArrayOutputStream()
+        val writer = OutputStreamWriter(outputStream)
+
+        try {
+            svg.stream(writer)
+            ctx.result(outputStream.toString(StandardCharsets.UTF_8.name()))
+        } catch (e: SVGGraphics2DIOException) {
+            e.printStackTrace()
+            ctx.result("")
+        } catch (e: UnsupportedEncodingException) {
+            e.printStackTrace()
+            ctx.result("")
+        }
+    }
+}
